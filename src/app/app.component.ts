ca858ad03c05f300ed3a0448aaf6b19a7b6be57f
prapti <prapti27verma@gmail.com>
2020-07-03 14:49:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private enrolservice:EnrollmentService){}
   title = 'angular-form-demo';
+  topics=['Angular', 'React', 'Vague'];
+  userModel=new User('','mohit@gmail.com',8732947932,'default','morning',true)
+  topicHasError=true;
+  validateTopic(value){
+    if(value == 'default')
+    {
+this.topicHasError=true;
+    }
+    else
+    this.topicHasError=false;
+  }
+
+
+  onSubmit()
+  {
+    this.enrolservice.enrol(this.userModel)
+    .subscribe(
+      data=>console.log('success',data),
+      error=>console.error('error',error)      
+    )
+
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor(
+        public name:String,
+        public email:String,
+        public phone:number,
+        public topic:String,
+        public timePreference:String,
+        public subscribe:boolean
+
+        
+    ){}
+}
