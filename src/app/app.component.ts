530a65de3688830fc640606daafca31597e3ddaa
prapti <prapti27verma@gmail.com>
2020-07-10 15:06:29 +0200

    error handling is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,10 @@ export class AppComponent {
   constructor(private enrolservice:EnrollmentService){}
   title = 'angular-form-demo';
   topics=['Angular', 'React', 'Vague'];
+  submitted =false;
   userModel=new User('','mohit@gmail.com',8732947932,'default','morning',true)
   topicHasError=true;
+  errorMsg='';
   validateTopic(value){
     if(value == 'default')
     {
@@ -23,13 +25,18 @@ this.topicHasError=true;
   }
 
 
-  onSubmit()
-  {
-    this.enrolservice.enrol(this.userModel)
-    .subscribe(
-      data=>console.log('success',data),
-      error=>console.error('error',error)      
-    )
+  // onSubmit()
+  // {
+  //   this.submitted=true;
+  //   this.enrolservice.enrol(this.userModel)
+  //   .subscribe(
+  //     data=>console.log('success',data),
+  //     error=>this.errorMsg=error.statusText   
+  //   )
 
+  // }
+  onSubmit(userForm)
+  {
+    console.log(userForm);
   }
 }
--- src/app/enrollment.service.ts
+++ src/app/enrollment.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import { User } from './user';
+import {throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class EnrollmentService {
- url='';
+  url='http://localhost:3000/enrol';
   constructor(private http:HttpClient) { }
   enrol(user:User)
   {
-return this.http.post<any>(this.url,user);
+      return this.http.post<any>(this.url,user)
+      .pipe(catchError(this.errorHandler)); 
+
+  }
+
+  errorHandler(error :HttpErrorResponse)
+  {
+    return throwError(error);
   }
 }
